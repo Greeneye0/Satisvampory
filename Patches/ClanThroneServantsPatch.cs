2dd3db6329920a8b17f53ee004d1f9e2067c826d
Satisvampory <satisvampory@users.noreply.github.com>
2026-09-01 16:27:58 -0400

    Ship 1.0.44: disable ClanShare throne servant patches that abort Burst on sit

--- Patches/ClanThroneServantsPatch.cs
+++ Patches/ClanThroneServantsPatch.cs
@@ -1,69 +1,11 @@
-using HarmonyLib;
-using ProjectM;
-using ProjectM.Network;
-using ProjectM.Shared.Systems;
-using Satisvampory.Services;
-using System;
-using Unity.Collections;
-using Unity.Entities;
-
 namespace Satisvampory.Patches
 {
     /// <summary>
-    /// Sit crashed the dedicated server: Burst SerializeAndSendServerEventsSystem
-    /// aborted after GetAllServants postfix wrote a native NativeList. Listing only
-    /// patches GetResponseEntries (vanilla Entry.Create into the response list).
+    /// ClanShare throne listing is disabled. Harmony postfix on GetResponseEntries
+    /// (ref FixedList4096Bytes) and GetAllServants (NativeList.Add) made Burst
+    /// SerializeAndSendServerEventsSystem abort the dedicated process on sit.
     /// </summary>
-    [HarmonyPatch(typeof(ServantInfoEventSystem_Server), nameof(ServantInfoEventSystem_Server.GetResponseEntries))]
-    public static class ClanThroneServantInfoPatch
+    public static class ClanThroneServantsPatch
     {
-        static void Postfix(Entity throneEntity, ref FixedList4096Bytes<ServantInfoEvent.Response.Entry> entries)
-        {
-            if (!Core.HasInitialized)
-                return;
-            try
-            {
-                ClanThroneServants.AddMissingEntries(throneEntity, ref entries);
-            }
-            catch (Exception e)
-            {
-                Core.LogException(e);
-            }
-        }
-    }
-
-    [HarmonyPatch(typeof(ServantInfoEventSystem_Server), nameof(ServantInfoEventSystem_Server.GetServantState))]
-    public static class ClanThroneServantStatePatch
-    {
-        static void Postfix(Entity servantEntity, ref ServantInfoEvent.Response.ServantState __result)
-        {
-            try
-            {
-                ClanThroneServants.HonorMissionState(servantEntity, ref __result);
-            }
-            catch (Exception e)
-            {
-                Core.LogException(e);
-            }
-        }
-    }
-
-    [HarmonyPatch(typeof(ServantMissionActionSystem), nameof(ServantMissionActionSystem.SendOnMissionJob_Execute))]
-    public static class ClanThroneSendOnMissionPatch
-    {
-        static void Prefix(ServantMissionActionSystem __instance, ref bool floorPlacementRestrictionsDisabled)
-        {
-            if (floorPlacementRestrictionsDisabled || !Core.HasInitialized)
-                return;
-            try
-            {
-                if (ClanThroneServants.StartQueryNeedsShare(__instance))
-                    floorPlacementRestrictionsDisabled = true;
-            }
-            catch (Exception e)
-            {
-                Core.LogException(e);
-            }
-        }
     }
 }
